ded6843e08ad86aa351d1bfe0a0da223c50277a2
mohan-sarath <45026070+mohan-sarath@users.noreply.github.com>
2018-11-14 15:01:35 +0800

    New

--- notebooks/Users/sarath.mohan@readify.net/TestNotebook.sql
+++ notebooks/Users/sarath.mohan@readify.net/TestNotebook.sql
@@ -1,1 +1,17 @@
 -- Databricks notebook source
+-- MAGIC %python
+-- MAGIC 
+-- MAGIC databricks secrets put --scope <scope-name> --key <key-name>
+
+-- COMMAND ----------
+
+databricks secrets list --scope jdbc
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC 
+-- MAGIC dbutils.fs.mount(
+-- MAGIC   source = "wasbs://data-bricks-test@azurestorageccount2018.blob.core.windows.net/",
+-- MAGIC   mount_point = "/mnt/mountsource",
+-- MAGIC   extra_configs = {"fs.azure.sas.data-bricks-test.azurestorageccount2018.blob.core.windows.net":dbutils.secrets.get(scope = "<scope-name>", key = "<key-name>")})
