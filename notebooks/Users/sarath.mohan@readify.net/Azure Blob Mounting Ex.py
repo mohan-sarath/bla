23e8aec8f55c36f4d2853d70e535261456621c86
mohan-sarath <45026070+mohan-sarath@users.noreply.github.com>
2018-11-15 16:43:18 +0800

    Final

--- notebooks/Users/sarath.mohan@readify.net/Azure Blob Mounting Ex.py
+++ notebooks/Users/sarath.mohan@readify.net/Azure Blob Mounting Ex.py
@@ -20,4 +20,25 @@ display(df1000.select("*"))
 
 # COMMAND ----------
 
-df1000 = spark.read.text("/mnt/testmount/10000 Sales Records.csv")
+df1000.createOrReplaceTempView("myview1000")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count (*) from myview1000
+
+# COMMAND ----------
+
+df1_5mil = spark.read.text("/mnt/testmount/1500000 Sales Records.csv")
+
+# COMMAND ----------
+
+df1_5mil.createOrReplaceTempView("myview1_5mil")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count (*) from myview1_5mil
+
+# COMMAND ----------
+
